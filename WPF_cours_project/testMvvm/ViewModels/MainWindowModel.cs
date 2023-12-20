826b1fe664e3832abc828ed0226e2ba19ae330cb
Shunks <nazar.n2812@gmail.com>
2023-12-21 00:46:55 +0200

    add combobox style

--- WPF_cours_project/testMvvm/ViewModels/MainWindowModel.cs
+++ WPF_cours_project/testMvvm/ViewModels/MainWindowModel.cs
@@ -41,25 +41,55 @@ namespace testMvvm.ViewModels
         //    get => buttons;
         //    set => Set(ref buttons, value);
         //}
-        
+
         //#endregion
 
         #endregion
-        
+
         #region Команды
 
+
+        #region addCars 
         public ICommand CreatAddWindowCommand { get; set; }
 
         private bool CanCreatAddWindowCommandExecute(object o) => true;
-        
+
         private void OnCreatAddWindowCommandExecute(object o)
         {
-            
+
             AddWindow addWindow = new AddWindow(dbTools);
             addWindow.Show();
-            
-        }
 
+        }
+        #endregion
+
+        #region addDrivers
+        public ICommand CreatAddWindowDriversCommand { get; set; }
+
+        private bool CanCreatAddWindowDriversCommandExecute(object o) => true;
+
+        private void OnCreatAddWindowDriversCommandExecute(object o)
+        {
+
+            AddWindow addWindow = new AddWindow(dbTools);
+            addWindow.Show();
+
+        }
+        #endregion
+
+        #region addSP
+        public ICommand CreatAddWindowSPCommand { get; set; }
+
+        private bool CanCreatAddWindowSPCommandExecute(object o) => true;
+
+        private void OnCreatAddWindowSPCommandExecute(object o)
+        {
+
+            AddWindow addWindow = new AddWindow(dbTools);
+            addWindow.Show();
+
+        }
+        #endregion
 
         public ICommand LoadDbCommand { get; set; }
 
@@ -78,6 +108,10 @@ namespace testMvvm.ViewModels
         {
             
             CreatAddWindowCommand = new LambdaCommand(OnCreatAddWindowCommandExecute, CanCreatAddWindowCommandExecute);
+            CreatAddWindowDriversCommand = new LambdaCommand(OnCreatAddWindowDriversCommandExecute, CanCreatAddWindowDriversCommandExecute);
+            CreatAddWindowSPCommand = new LambdaCommand(OnCreatAddWindowSPCommandExecute, CanCreatAddWindowSPCommandExecute);
+
+
             LoadDbCommand = new LambdaCommand(OnLoadDbCommandExecute, CanLoadDbCommandExecute);
             
 
